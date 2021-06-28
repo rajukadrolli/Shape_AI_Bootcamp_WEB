740098bb9698d414a989d6e0e0ad0ea8cd7f9659
rajukadrolli <86586574+rajukadrolli@users.noreply.github.com>
2021-06-28 12:50:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Info(){
+  return (
+    <div class="note">
+ <h1>Javascript and react.js</h1>
+ <p > A basic web dev react js bootcamp</p>
+ </div>
+  );
+  }
+
+export default Info;
